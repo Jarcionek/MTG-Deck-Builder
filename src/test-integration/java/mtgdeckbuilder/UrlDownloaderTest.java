4f2e83bd3a3dbffaac57f65f56b2f198afa25fc9
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-09-28 16:30:29 +0100

    renaming

--- src/test-integration/java/mtgdeckbuilder/UrlDownloaderTest.java
+++ src/test-integration/java/mtgdeckbuilder/UrlDownloaderTest.java
@@ -8,13 +8,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class CardsInfoDownloaderTest {
+public class UrlDownloaderTest {
 
-    private CardsInfoDownloader cardsInfoDownloader = new CardsInfoDownloader();
+    private UrlDownloader urlDownloader = new UrlDownloader();
 
     @Test
     public void downloadsFromUrl() throws FileNotFoundException {
-        String response = cardsInfoDownloader.download(new Url("http://api.mtgdb.info/cards/?fields=id,name&limit=2"));
+        String response = urlDownloader.download(new Url("http://api.mtgdb.info/cards/?fields=id,name&limit=2"));
 
         assertThat(response, is(equalTo("[{\"id\":1,\"name\":\"Ankh of Mishra\"},{\"id\":2,\"name\":\"Basalt Monolith\"}]")));
     }
