26ec0fb7bdc947be456b3dc5dfe01131acba9298
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-05 18:34:09 +0100

    implemented TagsManager

---
+++ src/main/java/mtgdeckbuilder/backend/TagsManager.java
@@ -0,0 +1,37 @@
+package mtgdeckbuilder.backend;
+
+import java.util.List;
+
+public class TagsManager {
+
+    private final TagFilesManager tagFilesManager;
+
+    public TagsManager(TagFilesManager tagFilesManager) {
+        this.tagFilesManager = tagFilesManager;
+    }
+
+    public void tag(String tagName, String cardName) {
+        List<String> loadedCards = tagFilesManager.load(tagName);
+
+        if (loadedCards.contains(cardName)) {
+            return;
+        }
+
+        loadedCards.add(cardName);
+
+        tagFilesManager.save(tagName, loadedCards);
+    }
+
+    public void untag(String tagName, String cardName) {
+        List<String> loadedCards = tagFilesManager.load(tagName);
+
+        loadedCards.remove(cardName);
+
+        tagFilesManager.save(tagName, loadedCards);
+    }
+
+    public List<String> getCards(String tagName) {
+        return tagFilesManager.load(tagName);
+    }
+
+}
