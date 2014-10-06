5464709d292a204c8fa18be35e5ad0fad31e5f0b
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-06 21:42:40 +0100

    application now uses system's "look and feel"

--- src/main/java/mtgdeckbuilder/Main.java
+++ src/main/java/mtgdeckbuilder/Main.java
@@ -3,10 +3,16 @@ package mtgdeckbuilder;
 import mtgdeckbuilder.frontend.MainFrame;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new RuntimeException("could not set system look and feel", e);
+        }
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
