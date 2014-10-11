54ec16d9c6e04eb76fab3ca458739b28a8abe735
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2014-10-11 23:05:46 +0100

    removed creating 3 test tags on startup

--- src/main/java/mtgdeckbuilder/Main.java
+++ src/main/java/mtgdeckbuilder/Main.java
@@ -4,8 +4,6 @@ import mtgdeckbuilder.frontend.MainFrame;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
@@ -14,15 +12,6 @@ public class Main {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             throw new RuntimeException("could not set system look and feel", e);
-        }
-
-        Config.tagsDirectory().mkdirs();
-        try {
-            new File(Config.tagsDirectory(), "test-tag-1.txt").createNewFile();
-            new File(Config.tagsDirectory(), "test-tag-2.txt").createNewFile();
-            new File(Config.tagsDirectory(), "test-tag-3.txt").createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
         SwingUtilities.invokeLater(new Runnable() {
